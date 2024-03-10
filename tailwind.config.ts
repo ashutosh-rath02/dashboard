05703b915ecdf83724fa4eb8c1a63b5165c1a839
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-03-10 20:30:31 +0530

    chore: update colors in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
+const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -101,6 +102,35 @@ const config: Config = {
       },
     },
   },
+  safelist: [
+    {
+      pattern:
+        /(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+    },
+    {
+      pattern:
+        /(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+    },
+    {
+      pattern:
+        /(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
+    },
+  ],
   plugins: [],
 };
 export default config;
